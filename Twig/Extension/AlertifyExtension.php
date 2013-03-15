23483f146b82a8eafca74ef308e10333190f8ffd
shadypierre <shadypierre@gmail.com>
2013-03-15 16:56:56 +0100

    - Removed deprecated call for getFlashes - Fix checkstyle

--- Twig/Extension/AlertifyExtension.php
+++ Twig/Extension/AlertifyExtension.php
@@ -1,30 +1,38 @@
 <?php
 
 namespace AppVentus\Awesome\AlertifyBundle\Twig\Extension;
-/**
- * Description of AlertifyExtension
- *
- * @author paul
- */
+
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
 
+/**
+ * AlertifyExtension
+ *
+ * @author Paul Andrieux
+ */
 class AlertifyExtension extends \Twig_Extension
 {
     protected $environment;
 
-
+    /**
+     * {@inheritDoc}
+     */
     public function initRuntime(\Twig_Environment $environment)
-        {
+    {
+        $this->environment = $environment;
+    }
 
-            $this->environment = $environment;
-        }
-
+    /**
+     * {@inheritDoc}
+     */
     public function getName()
     {
         return 'alertify';
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getFilters()
     {
         return array(
@@ -32,57 +40,58 @@ class AlertifyExtension extends \Twig_Extension
         );
     }
 
+    /**
+     * Alertify filter
+     *
+     * @param Session $session
+     * @return string
+     */
     public function alertifyFilter($session)
     {
         //CALL IT IN YOUR CTLR
-        //$this->container->get('session')->setFlash('modal', array('title' => 'test', 'body'=>'test body', 'button_class'=>'boutonmoyen'));
-        $flashes = $session->getFlashes();
+        $flashes = $session->getFlashBag()->all();
+
         $renders = array();
-        foreach($flashes as $key => $flash){
-                switch ($key) {
-                    case 'modal':
-                        $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:modal.html.twig',$flash);
-                        $session->removeFlash('modal');
-                        unset($flashes['modal']);
-                    break;
-                    case 'noty':
-                        $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:noty.html.twig', $flash);
-                        $session->removeFlash('noty');
-                        unset($flashes['noty']);
-                    break;
-                    case 'toastr':
-                        $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:toastr.html.twig', $flash);
-                        $session->removeFlash('toastr');
-                        unset($flashes['toastr']);
-                    break;
-                    case 'callback':
-                        $flash['body'] .= $this->environment->render('AvAwesomeAlertifyBundle:Modal:callback.html.twig', $flash);
-                        $session->removeFlash('callback');
-                        $session->setFlash($flash['type'], $flash);
-                        $renders[$key] = $this->alertifyFilter($session);
-                    break;
+        foreach ($flashes as $key => $flash) {
+            switch ($key) {
+                case 'modal':
+                    $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:modal.html.twig',$flash);
+                    $session->removeFlash('modal');
+                    unset($flashes['modal']);
+                break;
+                case 'noty':
+                    $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:noty.html.twig', $flash);
+                    $session->removeFlash('noty');
+                    unset($flashes['noty']);
+                break;
+                case 'toastr':
+                    $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:toastr.html.twig', $flash);
+                    $session->removeFlash('toastr');
+                    unset($flashes['toastr']);
+                break;
+                case 'callback':
+                    $flash['body'] .= $this->environment->render('AvAwesomeAlertifyBundle:Modal:callback.html.twig', $flash);
+                    $session->removeFlash('callback');
+                    $session->setFlash($flash['type'], $flash);
+                    $renders[$key] = $this->alertifyFilter($session);
+                break;
                 default:
-
-
-                if(is_array($flash)){
-                        $renders[$key] = $this->environment->render($key.'.html.twig',$flash);
+                    if (is_array($flash)) {
+                            $renders[$key] = $this->environment->render($key.'.html.twig',$flash);
+                            $session->removeFlash($key);
+                    }
+                    if (! is_array($flash)) {
+                        $value = array('type' => 'success', 'layout' => 'bottom-left' ,'body' => $flash);
+                        $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:toastr.html.twig', $value);
                         $session->removeFlash($key);
-                }
+                    }
 
-                if(!is_array($flash)){
-                    $value = array('type'=>'success', 'layout'=>'bottom-left' ,'body'=>$flash);
-                    $renders[$key] = $this->environment->render('AvAwesomeAlertifyBundle:Modal:toastr.html.twig', $value);
-                    $session->removeFlash($key);
-                }
-
-
-                        unset($flashes[$key]);
-                    break;
+                    unset($flashes[$key]);
+                break;
             }
         }
-        return implode(" ", $renders);
+
+        return implode(' ', $renders);
     }
 
 }
-
-?>
