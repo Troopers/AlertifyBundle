c8a158621cf1d998f8a622cfae1005735441c9ae
Leny BERNARD <leny@troopers.email>
2016-10-20 00:25:18 +0200

    :art: use xml for the service definition

--- DependencyInjection/TroopersAlertifyExtension.php
+++ DependencyInjection/TroopersAlertifyExtension.php
@@ -22,8 +22,8 @@ class TroopersAlertifyExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
         $container->setParameter('troopers_alertify', $config);
         $container->setParameter('troopers_alertify.contexts', $config['contexts']);
